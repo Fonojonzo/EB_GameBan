aa89fc633448a33d6535a682b6c5ae1be1663ec8
iExTno <132175289+iExTno@users.noreply.github.com>
2023-10-17 19:25:40 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -102,5 +102,6 @@ local Ban = {
 	[5132741764] = {Motivo = "flw"}, -- ''ExTnoCreK'' 
 	[2545572927] = {Motivo = "não fuja"}, -- achou que sair iria salvar a propria pele trol de comando all 
 	[4078842018] = {Motivo = "flw"}, -- troll matando geral aproveitando q n tem comandos 
+	[3960556555] = {Motivo = "flw"}, -- troll matando geral aproveitando q n tem comandos 
 }
 return Ban 
