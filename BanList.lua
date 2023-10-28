30fbb6a02325f57908caf085b07171466b85e5b7
iExTno <132175289+iExTno@users.noreply.github.com>
2023-10-28 17:00:48 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -127,6 +127,7 @@ local Ban = {
 	[4935074060]  = {Motivo = "flw"}, -- matando gera
 	[1551853094]  = {Motivo = "Gamepass"}, -- tentando enganar os helper
 	[2983325277]  = {Motivo = "eu te avisei"}, -- matando as pessoas do nada
+	[2403238884]  = {Motivo = "eu te avisei"}, -- 
 	
 }
 return Ban 
