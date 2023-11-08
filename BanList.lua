e7ab4250cd1c632b69a351a8be88b0dea5fbbfc9
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2023-11-08 14:03:35 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -141,6 +141,8 @@ local Ban = {
 	[2908376678] = {Motivo = "EB CLONADA, EXCLUA A EB FAKE PARA SAIR DO BAN"}, --  eb clone
 	[2451275596] = {Motivo = "EB CLONADA, EXCLUA A EB FAKE PARA SAIR DO BAN"}, --  eb clone
 	[3048011540] = {Motivo = "Quer aprender vai para a escola."}, -- chatão
+	[3856155138] = {Motivo = "Flw"}, -- change all
+	
 	
 	
 	
