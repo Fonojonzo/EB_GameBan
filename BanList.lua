528d15ded713d82de4f497acd3680ded6a8065e7
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2023-11-18 15:34:37 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -137,7 +137,6 @@ local Ban = {
 	[15210477327] = {Motivo = "EB CLONADA, EXCLUA A EB FAKE PARA SAIR DO BAN"}, -- eb clone
 	[1163468836] = {Motivo = "EB CLONADA, EXCLUA A EB FAKE PARA SAIR DO BAN"}, --  eb clone
 	[2054466194] = {Motivo = "EB CLONADA, EXCLUA A EB FAKE PARA SAIR DO BAN"}, --  eb clone
-	[2908376678] = {Motivo = "EB CLONADA, EXCLUA A EB FAKE PARA SAIR DO BAN"}, --  eb clone
 	[2451275596] = {Motivo = "EB CLONADA, EXCLUA A EB FAKE PARA SAIR DO BAN"}, --  eb clone
 	[3048011540] = {Motivo = "Quer aprender vai para a escola."}, -- chatão
 	[3856155138] = {Motivo = "Flw"}, -- change all
