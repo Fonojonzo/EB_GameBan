ba1cb7ee70517fee54799eac64b4f45d5c6b7ae8
iExTno <132175289+iExTno@users.noreply.github.com>
2023-11-20 23:37:19 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -161,8 +161,8 @@ local Ban = {
 	[4582627541] = {Motivo = "flw"}, -- change all
 	[2950992551] = {Motivo = "flw"}, -- setlevel %
 	[3856155138] = {Motivo = "flw"}, -- setlevel %
-	[4961359008] = {Motivo = "tu vai ficar banido até eu resolver"}, -- nuel copiando meu nick	
-	[5182375266] = {Motivo = "tu vai ficar banido até eu resolver"}, -- nuel copiando meu nick pra trolar
+	[4961359008] = {Motivo = ""}, -- nuel copiando meu nick	
+	[5182375266] = {Motivo = ""}, -- nuel copiando meu nick pra trolar
 	
 	
 	
