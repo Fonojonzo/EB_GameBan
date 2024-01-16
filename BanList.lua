e190acbb75a3d1d4c47787282c0d61499b2cc994
iExTno <132175289+iExTno@users.noreply.github.com>
2024-01-16 16:25:08 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -3,6 +3,7 @@ local Ban = {
 		[3615676414] = {Motivo = "flw"}, -- p3u
 		[1672825547] = {Motivo = "¿"}, -- cobaia do p3u
 		[1968730428] = {Motivo = "flw"}, -- ex helper
+		[2963030044] = {Motivo = "flw"}, -- ex helper
 		[1919390511] = {Motivo = "Wild fire all"},
 		[3380344108] = {Motivo = "Mutando todos do jogo e tirando patente"},
 		[3715134739] = {Motivo = "Unrank all, ban all"},
@@ -206,7 +207,7 @@ local Ban = {
 		[5281676202] = {Motivo = "flw"}, --   ban all
 		[4979128725] = {Motivo = "Fracassado"}, --   exploit / comandos all 
 		[1968730428] = {Motivo = "flw"}, -- ex helper ed3
-		
+		[2963030044] = {Motivo = "flw"}, -- ex helper
 		
 	},
 
