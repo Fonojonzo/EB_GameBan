dfc47ccafd9545273f296147c905a7932747817a
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2024-02-21 23:15:59 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -219,6 +219,8 @@ local Ban = {
 		[2384089789] = {Motivo = "Não foi boa ideia dar deslike "}, -- dando deslike no eb custom char assim que tomou ban por abusar
 		[1244608487] = {Motivo = "eb clonada"}, -- eb clonada
 		[3209039131] = {Motivo = "eb clonada"}, -- eb clonada
+		[4975623840] = {Motivo = "racismo"}, -- racismo
+		
 	},
 	['Ban_Amigos'] = {
 		[5275182514] = {Motivo = "Flw"}, 
