4c80469e34964047e7cbc60a4d022a1bc7dde882
iExTno <132175289+iExTno@users.noreply.github.com>
2024-03-04 12:06:03 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -9,6 +9,7 @@ local Ban = {
 		[4325122085] = {Motivo = "flw"}, -- dando jail all q nem retardado
 		[2601082506] = {Motivo = "flw"}, -- trocando de conta no dc
 		[914914325] = {Motivo = "flw"}, -- pedrinhouuuuuuu
+		[3050985842] = {Motivo = "flw"}, -- 
 		[563210570] = {Motivo = "flw"}, -- trolando all
 		[3670248170] = {Motivo = "flw"}, -- usando comandos all
 		[4035850120] = {Motivo = "flw"}, -- resolveu sair do eb e ficou dando gear pra varios player
