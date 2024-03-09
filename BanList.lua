99f71b949560ba242647f1f9df43ac9de9299832
iExTno <132175289+iExTno@users.noreply.github.com>
2024-03-09 03:57:11 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -186,7 +186,6 @@ local Ban = {
 		[3627457285] = {Motivo = "flw"}, --   dando setrank all
 		[4303100633] = {Motivo = "flw"}, --   mandando foto de pessoas sendo cortada no servidor do dc
 		[3062662803] = {Motivo = "flw"}, --   usando comandos all
-		[4910910227] = {Motivo = "flw"}, --   matando geral de gojo
 		[3410284177] = {Motivo = "flw"}, --   mandando comprovante falso de compras
 		[4132170652] = {Motivo = "flw"}, --   change abcd
 		[2692954000] = {Motivo = "noob"}, --   tentando fazer ameaças
