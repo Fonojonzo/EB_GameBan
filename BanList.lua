263a6e89fe7df22b48c3626bcdfc15e1bca9edf4
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2024-03-10 21:57:06 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -13,7 +13,6 @@ local Ban = {
 		[2601082506] = {Motivo = "flw"}, -- trocando de conta no dc
 		[563210570] = {Motivo = "flw"}, -- trolando all
 		[3670248170] = {Motivo = "flw"}, -- usando comandos all
-		[4035850120] = {Motivo = "flw"}, -- resolveu sair do eb e ficou dando gear pra varios player
 		[1919390511] = {Motivo = "Wild fire all"},
 		[1449720480] = {Motivo = "Atenção que tanto queria"}, -- troll dc
 		[1835213349] = {Motivo = "flw"}, -- copiando nick extnocrak 
