d405fcc7e996f772936fa0bcdb3750fe60f66fe5
iExTno <132175289+iExTno@users.noreply.github.com>
2024-05-12 13:16:33 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -1,7 +1,6 @@
 local Ban = {
 	['Ban'] = { 
 		[3615676414] = {Motivo = "flw"}, -- Vitor
-		[3288350628] = {Motivo = "flw"}, -- esquisito do dc
 		[1672825547] = {Motivo = "flw"}, -- cobaia do vitor
 		[1599379141] = {Motivo = "flw"}, -- tentativa de raid no dc
 		[982811126] = {Motivo = "flw"}, -- cumplice pedrinhouu
