c540f564ff58a85116124f6a10ca9faca19f5137
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2024-05-20 03:55:12 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -227,7 +227,6 @@ local Ban = {
 		[4826162014] = {Motivo = "Vai correr atrás de mulher em outra EB."}, --   wanderson escravo da mandy
 		[5496751234] = {Motivo = "Comprovante falso."},
 		[2389965328] = {Motivo = "Divulgando EB no discord"},
-		[4588202359] = {Motivo = "Chantagem/ Clone da EB"}, -- Chantageando para ganhar cargo com cópia da EB
 		[3290770099] = {Motivo = "Chantagem/ Clone da EB"}, -- Chantageando para ganhar cargo com cópia da EB
 		[5345800906] = {Motivo = "roblox negativo"}, -- trolando fingindo compra --
 		[2984414856] = {Motivo = "EB CLONADA / EXCLUA A EB PARA TIRAR O BAN."}, -- trolando fingindo compra
