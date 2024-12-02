7e03d746217f26ab93f903b1d3160467a504e483
iExTno <132175289+iExTno@users.noreply.github.com>
2024-12-02 01:30:06 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -1,7 +1,7 @@
 local Ban = {
 	['Ban'] = { 
 		[7541015985] = {Motivo = "flw"}, -- fonojonzoalt
-		--[4943155186] = {Motivo = "EB clonada."}, -- EASY_BR00
+		[4943155186] = {Motivo = "EB clonada."}, -- EASY_BR00
 		[3615676414] = {Motivo = "flw"}, -- Vitor
 		[5401109450] = {Motivo = "flw"}, -- P1QUENO_BISONHO exploit de armas
 		[1972731647] = {Motivo = "flw"}, -- raul
