3ad22a91f16c368261c3032d8f524c5599e36a1d
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-02-13 23:35:35 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -3,6 +3,7 @@ local Ban = {
 
 
 		
+		[3592165962] = {Motivo = "flw"}, -- defendendo jogo clonado
 		[7209935473] = {Motivo = "flw"}, -- apagou 10 tickets de venda
 		[5485890891] = {Motivo = "Comprovante falso."}, -- flw men
 		[3586882255] = {Motivo = "Comprovante falso."}, -- flw men
