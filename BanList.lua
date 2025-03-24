7c8b39963b5513d61d9bb8979768f2fc9055a08d
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-03-24 00:32:31 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -1,6 +1,9 @@
 local Ban = {
 	['Ban'] = { 
+
 		
+		[2267845183] = {Motivo = "Jogo Clonado"}, -- Jogo Clonado
+		[18668065416] = {Motivo = "Jogo Clonado"}, -- Jogo Clonado
 		[2463225312] = {Motivo = "Comprovante falso."}, -- comprovante falso
 		[4255372027] = {Motivo = "Comprovante falso."}, -- comprovante falso
 		[1345097930] = {Motivo = "Estorno"}, -- estornou todas as compras steam
