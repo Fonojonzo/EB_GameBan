3799e71336a298fc13fa68767ad60247cbfde32f
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2025-07-21 18:01:06 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -2,6 +2,7 @@ local Ban = {
 	['Ban'] = { 
 
 
+		[5138312115] = {Motivo = "Comprovante Falso."}, -- comprovante falso
 		
 		[3322067390] = {Motivo = "Venda de contas"}, -- lobinha_vr venda de conta.
 		[3517272121] = {Motivo = "Venda de contas"}, -- lobinha_vr venda de conta.
