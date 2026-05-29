baa0a013754979875890712a8d5c97c1e5e1810c
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2026-05-29 17:36:30 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -2,6 +2,7 @@ local Ban = {
 	['Ban'] = { 
 
 
+		[8651321555] = {Motivo = 'flw'}, -- Golpe comprovante agendado
 		[10106924487] = {Motivo = 'Poupando trabalho'}, -- reclamando e banindo quem chamou de "mahoraga gordo" 90 dias, depois chorando falando que não vai jogar mais
 		[7251603416] = {Motivo = 'flw'}, -- eb ta falindo kkkk
 		[10448026976] = {Motivo = 'flw'}, -- ban all
